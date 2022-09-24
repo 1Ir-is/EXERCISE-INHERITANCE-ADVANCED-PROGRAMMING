ee3153a4aa30d493f081e11a7f63f92b528586fb
IriS <mhuynk1005@gmail.com>
2022-09-24 17:36:51 +0700

    Problem 3

--- Problem 3/Program.cs
+++ Problem 3/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Problem_3;
+
+var inputLine = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+var firstName = inputLine[0];
+var lastName = inputLine[1];
+var facultyNumber = inputLine[2];
+Student student;
+try
+{
+    student = new Student(firstName, lastName, facultyNumber);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    return;
+}
+
+inputLine = Console.ReadLine()
+    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+firstName = inputLine[0];
+lastName = inputLine[1];
+var weekSalary = double.Parse(inputLine[2]);
+var hoursPerDay = double.Parse(inputLine[3]);
+Worker worker;
+try
+{
+    worker = new Worker(firstName, lastName, weekSalary, hoursPerDay);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    return;
+}
+
+Console.WriteLine(student);
+Console.WriteLine(worker);
